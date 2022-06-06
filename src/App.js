ea9ba63adaa986cbe0c94600f073a41ab829f0c5
Clinton Renton <93984913+YonTracks@users.noreply.github.com>
2022-06-06 18:49:16 +1000

    added login page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Proposal from "./pages/Proposal";
 import moralisLogo from "./images/Moralis.png";
+import { useMoralis } from "react-moralis";
 import { ConnectButton } from "web3uikit";
 
 const App = () => {
+  const { isAuthenticated } = useMoralis();
   return (
-    <div className="main-window">
+    <>
       <div className="header">
         <img width="160px" src={moralisLogo} alt="logo" />
-        <ConnectButton className="connectButton"/>
+        <div className="connectButton">
+        <ConnectButton />
+        </div>
       </div>
+      {isAuthenticated ? (
+        
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/proposal" element={<Proposal />} />
       </Routes>
-    </div>
+      ) : (
+        <div style={{backgroundColor: "transparent"}} className="loginPage">
+          <div className="content">
+          <h1 style={{font: "bold", fontSize: "43px",color: "white"}}>Welcome to YonDAO</h1>
+        </div>
+        </div>
+      )}
+    </>
   );
 };
 
